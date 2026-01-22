a6b5029a83b41f063f982fc1349bf26e79e50c88
Luis Pastor Martinez <lfpmda@gmail.com>
2026-01-22 16:47:32 +0000

    Fix utils __init__ imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,16 +1,13 @@
 from .data_utils import limpiar_telefono, limpiar_fecha
-# from .excel_utils import load_dataframes_local, save_dataframe_local
-from .firestore_utils import load_dataframes_firestore, save_dataframe_firestore, delete_document_firestore
+from .firestore_utils import load_dataframes_firestore, delete_document_firestore, get_firestore_client
 from .helpers import convert_to_firestore_type, safe_select_index
 
 __all__ = [
     'limpiar_telefono',
     'limpiar_fecha',
-    'load_dataframes_local',
-    'save_dataframe_local',
     'load_dataframes_firestore',
-    'save_dataframe_firestore',
     'delete_document_firestore',
+    'get_firestore_client',
     'convert_to_firestore_type',
     'safe_select_index'
 ]

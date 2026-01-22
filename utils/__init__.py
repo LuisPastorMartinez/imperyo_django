0166671a3ff95f24a94373e6eeb3ee5fa47afad4
Luis Pastor Martinez <lfpmda@gmail.com>
2026-01-22 16:53:01 +0000

    Fix missing Firestore imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,11 @@
 from .data_utils import limpiar_telefono, limpiar_fecha
-from .firestore_utils import load_dataframes_firestore, delete_document_firestore, get_firestore_client
+from .firestore_utils import load_dataframes_firestore, get_firestore_client
 from .helpers import convert_to_firestore_type, safe_select_index
 
 __all__ = [
     'limpiar_telefono',
     'limpiar_fecha',
     'load_dataframes_firestore',
-    'delete_document_firestore',
     'get_firestore_client',
     'convert_to_firestore_type',
     'safe_select_index'
